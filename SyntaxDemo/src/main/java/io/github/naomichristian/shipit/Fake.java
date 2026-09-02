1709cb5a96059f1d42fb5a0e172292d41e5bee7d
Naomi <naomichristian19@yahoo.com>
2026-09-01 20:48:49 -0400

    updated Sept 1

---
+++ SyntaxDemo/src/main/java/io/github/naomichristian/shipit/Fake.java
@@ -0,0 +1,14 @@
+package io.github.naomichristian.shipit;
+
+public class Fake {
+    public int age;
+    public String firstName;
+    public String lastName;
+
+    public Fake(int age, String firstName, String lastName){
+        this.age = age;
+    }
+    public int getAge(){
+        return age;
+    }
+}
